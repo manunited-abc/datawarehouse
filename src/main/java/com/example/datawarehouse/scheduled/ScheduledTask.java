dc47b2a4c5b3f30e3c6ecc26875eb40d6292e651
manunited-abc <19130071@st.hcmuaf.edu.vn>
2022-09-20 12:08:58 +0700

    change zone time

--- src/main/java/com/example/datawarehouse/scheduled/ScheduledTask.java
+++ src/main/java/com/example/datawarehouse/scheduled/ScheduledTask.java
@@ -17,7 +17,7 @@ public class ScheduledTask {
     @Autowired
     DataService dataService;
     private static final Logger LOGGER = LoggerFactory.getLogger(ScheduledTask.class);
-    @Scheduled(cron = "00 42 17 * * ?")
+    @Scheduled(cron = "00 10 5 * * ?")
     public void scheduleTaskWithCronExpression(){
         LocalDateTime date = LocalDateTime.now(ZoneId.of("Asia/Ho_Chi_Minh"));
         dataService.writeToCSV(date);

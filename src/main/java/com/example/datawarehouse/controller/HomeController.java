ee2aad5421813f37f401abec57cd42e5c1534ae3
manunited-abc <19130071@st.hcmuaf.edu.vn>
2022-09-20 12:01:55 +0700

    change zone time

--- src/main/java/com/example/datawarehouse/controller/HomeController.java
+++ src/main/java/com/example/datawarehouse/controller/HomeController.java
@@ -5,13 +5,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
 @Controller
 public class HomeController {
     @GetMapping(value={"home","/","index"})
     public String home(Model model){
-        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime now = LocalDateTime.now(ZoneId.of("Asia/Ho_Chi_Minh"));
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd,MM,yyyy HH:mm:ss");
         String time = now.format(formatter);
         model.addAttribute("currentDate",time);
